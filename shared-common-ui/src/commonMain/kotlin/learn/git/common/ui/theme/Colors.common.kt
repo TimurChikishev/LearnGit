fced8be08feb6dcc599415f00aef7de66e0a2958
Тимур Чикишев <t.chikishev@ktsstudio.ru>
2023-04-20 23:00:28 +0300

    [TASK-1] Setup theme

--- shared-common-ui/src/commonMain/kotlin/learn/git/common/ui/theme/Colors.common.kt
+++ shared-common-ui/src/commonMain/kotlin/learn/git/common/ui/theme/Colors.common.kt
@@ -3,8 +3,8 @@ package learn.git.common.ui.theme
 import dev.icerock.moko.graphics.Color as KColor
 import androidx.compose.material3.ColorScheme
 import androidx.compose.ui.graphics.Color
-import learn.git.res.MR
 import dev.icerock.moko.resources.ColorResource
+import learn.git.res.MR
 
 fun KColor.asCompose(): Color = Color(color = argb.toInt())
 fun ColorResource.Single.asCompose(): Color = color.asCompose()
@@ -45,30 +45,37 @@ enum class Colors(
         onPrimary = MR.colors.onPrimaryLight.asCompose(),
         primaryContainer = MR.colors.primaryContainerLight.asCompose(),
         onPrimaryContainer = MR.colors.onPrimaryContainerLight.asCompose(),
-        inversePrimary = MR.colors.white.asCompose(),
+        inversePrimary = MR.colors.inversePrimaryLight.asCompose(),
+
         secondary = MR.colors.secondaryLight.asCompose(),
         onSecondary = MR.colors.onSecondaryLight.asCompose(),
         secondaryContainer = MR.colors.secondaryContainerLight.asCompose(),
         onSecondaryContainer = MR.colors.onSecondaryContainerLight.asCompose(),
+
         tertiary = MR.colors.tertiaryLight.asCompose(),
         onTertiary = MR.colors.onTertiaryLight.asCompose(),
         tertiaryContainer = MR.colors.tertiaryContainerLight.asCompose(),
         onTertiaryContainer = MR.colors.onTertiaryContainerLight.asCompose(),
+
         error = MR.colors.errorLight.asCompose(),
         onError = MR.colors.onErrorLight.asCompose(),
         errorContainer = MR.colors.errorContainerLight.asCompose(),
         onErrorContainer = MR.colors.onErrorContainerLight.asCompose(),
+
         background = MR.colors.backgroundLight.asCompose(),
         onBackground = MR.colors.onBackgroundLight.asCompose(),
+
         surface = MR.colors.surfaceLight.asCompose(),
         onSurface = MR.colors.onSurfaceLight.asCompose(),
         surfaceVariant = MR.colors.surfaceVariantLight.asCompose(),
         onSurfaceVariant = MR.colors.onSurfaceVariantLight.asCompose(),
-        inverseSurface = MR.colors.surfaceLight.asCompose(),
-        inverseOnSurface = MR.colors.onSurfaceLight.asCompose(),
-        surfaceTint = MR.colors.outlineDark.asCompose(),
+        inverseSurface = MR.colors.inverseSurfaceLight.asCompose(),
+        inverseOnSurface = MR.colors.inverseOnSurfaceLight.asCompose(),
+        surfaceTint = MR.colors.surfaceTintLight.asCompose(),
+
         outline = MR.colors.outlineLight.asCompose(),
         outlineVariant = MR.colors.outlineVariantLight.asCompose(),
+
         scrim = MR.colors.white.asCompose(),
     ),
     Dark(
@@ -76,30 +83,37 @@ enum class Colors(
         onPrimary = MR.colors.onPrimaryDark.asCompose(),
         primaryContainer = MR.colors.primaryContainerDark.asCompose(),
         onPrimaryContainer = MR.colors.onPrimaryContainerDark.asCompose(),
+        inversePrimary = MR.colors.inversePrimaryDark.asCompose(),
+
         secondary = MR.colors.secondaryDark.asCompose(),
         onSecondary = MR.colors.onSecondaryDark.asCompose(),
         secondaryContainer = MR.colors.secondaryContainerDark.asCompose(),
         onSecondaryContainer = MR.colors.onSecondaryContainerDark.asCompose(),
+
         tertiary = MR.colors.tertiaryDark.asCompose(),
         onTertiary = MR.colors.onTertiaryDark.asCompose(),
         tertiaryContainer = MR.colors.tertiaryContainerDark.asCompose(),
         onTertiaryContainer = MR.colors.onTertiaryContainerDark.asCompose(),
+
         error = MR.colors.errorDark.asCompose(),
         onError = MR.colors.onErrorDark.asCompose(),
         errorContainer = MR.colors.errorContainerDark.asCompose(),
         onErrorContainer = MR.colors.onErrorContainerDark.asCompose(),
+
         background = MR.colors.backgroundDark.asCompose(),
         onBackground = MR.colors.onBackgroundDark.asCompose(),
+
         surface = MR.colors.surfaceDark.asCompose(),
         onSurface = MR.colors.onSurfaceDark.asCompose(),
         surfaceVariant = MR.colors.surfaceVariantDark.asCompose(),
         onSurfaceVariant = MR.colors.onSurfaceVariantDark.asCompose(),
-        inverseSurface = MR.colors.surfaceDark.asCompose(),
-        inverseOnSurface = MR.colors.onSurfaceDark.asCompose(),
+        inverseSurface = MR.colors.inverseSurfaceDark.asCompose(),
+        inverseOnSurface = MR.colors.inverseOnSurfaceDark.asCompose(),
+        surfaceTint = MR.colors.surfaceTintDark.asCompose(),
+
         outline = MR.colors.outlineDark.asCompose(),
         outlineVariant = MR.colors.outlineVariantDark.asCompose(),
-        surfaceTint = MR.colors.black.asCompose(),
-        inversePrimary = MR.colors.black.asCompose(),
+
         scrim = MR.colors.black.asCompose(),
     );
 
@@ -127,8 +141,8 @@ enum class Colors(
             onSurface = onSurface,
             surfaceVariant = surfaceVariant,
             onSurfaceVariant = onSurfaceVariant,
-            inverseSurface = surface,
-            inverseOnSurface = onSurface,
+            inverseSurface = inverseSurface,
+            inverseOnSurface = inverseOnSurface,
             outline = outline,
             outlineVariant = outlineVariant,
             surfaceTint = surfaceTint,
